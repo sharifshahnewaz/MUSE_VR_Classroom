8a2b61ff88d4bc3daee3c1c845c7cb04ff35a746
Jan Tameta <tametaj1@lions.tcnj.edu>
2019-06-26 13:55:02 -0400

    New files added

---
+++ Assets/Scripts/BlinkScript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkScript : MonoBehaviour
+{
+    public GameObject Cr1, Cr2, Cr3;
+    public  Material m_Material;
+    public int classRoomCounter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        classRoomCounter = 1;
+
+        Cr1.transform.gameObject.SetActive(true);
+        Cr2.transform.gameObject.SetActive(false);
+        Cr3.transform.gameObject.SetActive(false);
+    }
+    
+        // Update is called once per frame
+        void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            classRoomCounter++;
+            if (classRoomCounter > 3)
+                classRoomCounter = 1;
+
+            if (classRoomCounter == 1)
+            {
+                Cr1.transform.gameObject.SetActive(true);
+                Cr2.transform.gameObject.SetActive(false);
+                Cr3.transform.gameObject.SetActive(false);
+            }
+            if (classRoomCounter == 2)
+            {
+                Cr1.transform.gameObject.SetActive(false);
+                Cr2.transform.gameObject.SetActive(true);
+                Cr3.transform.gameObject.SetActive(false);
+            }
+            if (classRoomCounter == 3)
+            {
+                Cr1.transform.gameObject.SetActive(false);
+                Cr2.transform.gameObject.SetActive(false);
+                Cr3.transform.gameObject.SetActive(true);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            m_Material.color = Color.red;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            m_Material.color = Color.blue;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            m_Material.color = Color.black;
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            m_Material.color = Color.white;
+        }
+    }
+}
---
+++ Assets/Scripts/ChangeSeat.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSeat : MonoBehaviour
+{
+    public GameObject seat1, seat2, seat3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    void Update() // currently uses keypad values to change the seating of the active person.
+    {
+        if (Input.GetKeyDown("[7]"))
+        {
+            print("changing to base position");
+            this.transform.position = seat1.transform.position;
+        }
+        if (Input.GetKeyDown("[8]"))
+        {
+            print("changing to 1st position");
+            this.transform.position = seat2.transform.position;
+        }
+        if (Input.GetKeyDown("[9]"))
+        {
+            print("changing to 2nd position");
+
+            this.transform.position = seat3.transform.position;
+        }
+    }
+}
