6fa54fca0317d325113593b357e8a9167c459b12
Alyssa Popper <alyssapopper@Alyssas-MacBook-Pro-2.local>
2019-07-24 14:36:15 -0400

    Adding teacher animaitons

---
+++ Assets/Scripts/TeacherController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeacherController : MonoBehaviour
+{
+    bool walkBackAllowed =false;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        StartCoroutine("walk");
+    }
+
+   
+    IEnumerator walk()
+    {
+        while (true)
+        {
+            int time = Random.Range(5, 10);
+            if (time > 8)
+            {
+                if (walkBackAllowed == false)
+                {
+                    anim.SetBool("isWalkingRight", true);
+                    yield return new WaitForSeconds(0.1F);
+                    walkBackAllowed = true;
+                    anim.SetBool("isWalkingRight", false);
+                }
+                else {
+                    anim.SetBool("isWalkingLeft", true);
+                    yield return new WaitForSeconds(0.1F);
+                    walkBackAllowed = false;
+                    anim.SetBool("isWalkingLeft", false);
+                }
+            }
+            else
+            {
+                anim.SetBool("Point", true);
+                yield return new WaitForSeconds(1);
+                anim.SetBool("Point", false);
+            }
+            yield return new WaitForSeconds(time);
+        }
+    }
+}
